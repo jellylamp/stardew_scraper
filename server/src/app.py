80c77cd3f38aaa54e0893169e39b819d38c75af6
Caroline Galecki <carolinegalecki@gmail.com>
2020-01-20 00:07:06 -0500

    Added loved gifts.

--- server/src/app.py
+++ server/src/app.py
@@ -23,20 +23,41 @@ def _get_birthday(name):
     soup = soup_utils.make_soup(response.text)
     search_success = soup_utils.was_search_successful(soup)
     if search_success:
-        # TODO HANDLE when a character isn't passed in. i.e. barn
+        # TODO HANDLE when a character isn't passed in. i.e. barn. Right now it just asks you to repeat yourself.
 
         birthday = character.get_birthday(soup)
         return {'birthday': birthday}
     else:
         return {'birthday': f'Name {name} not found'}
 
+@app.route('/api/v1/best_gifts', methods=['GET'])
+def get_best_gifts():
+    name = request.args.get('name')
+    return jsonify(_get_best_gifts(name))
+
+def _get_best_gifts(name):
+    url = f'{API_URL}{SEARCH_API}{name}'
+    response = requests.get(url)
+    soup = soup_utils.make_soup(response.text)
+    search_success = soup_utils.was_search_successful(soup)
+    if search_success:
+        # TODO HANDLE when a character isn't passed in. i.e. barn. Right now it just asks you to repeat yourself.
+
+        best_gifts = character.get_best_gifts(soup)
+        return {'best_gifts': best_gifts}
+    else:
+        return {'best_gifts': f'Name {name} not found'}
+
 @app.route('/api/v1/post_fulfillment', methods=['POST'])
 def post_fulfillment():
     data = request.get_json()
     intent = data.get('queryResult').get('action')
+    name_param = data.get('queryResult').get('parameters').get('name')
 
     if intent == 'birthday':
-        response = _get_birthday(data.get('queryResult').get('parameters').get('name')).get('birthday')
+        response = _get_birthday(name_param).get('birthday')
+    elif intent == 'best_gifts':
+        response = _get_best_gifts(name_param).get('best_gifts')
 
     response_dict = {
         'fulfillmentText' : response
--- server/src/character.py
+++ server/src/character.py
@@ -7,3 +7,12 @@ class Character():
         season = birthday_soup.find('a').attrs['title']
         date = birthday_soup.contents[1].strip()
         return f'{season} {date}'
+
+    def get_best_gifts(self, soup):
+        gift_soup = SoupUtils.get_info_section_by_name(self, soup, 'Best Gifts:')
+        gift_list = []
+
+        for gift in gift_soup.find_all('a'):
+            gift_list.append(gift.attrs['title'])
+
+        return ", ".join(gift_list[:-2] + [", and ".join(gift_list[-2:])])
